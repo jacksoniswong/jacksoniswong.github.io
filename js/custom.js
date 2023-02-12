24e2561407a5dd5aafd23a40fbc7f18d4646a989
Jackson Wong <jacksonwong13@hotmail.com>
2023-02-12 16:29:07 +1300

    adding jquery

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+const images = document.querySelectorAll(".images img");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modalImg");
+const modalTxt = document.querySelector(".modalTxt");
+const close = document.querySelector(".close");
+
+images.forEach((image) => {
+  image.addEventListener("click", () => {
+    modalImg.src = image.src;
+    modalTxt.innerHTML = image.alt;
+    modal.classList.add("appear");
+
+    close.addEventListener("click", () => {
+      modal.classList.remove("appear");
+    });
+  });
+});
